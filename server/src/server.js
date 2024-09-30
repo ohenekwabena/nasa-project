9897a2b0477ba2c6276ea4c96bf7c411a85bd30e
ohenekwabena <adjeiasarejesse@gmail.com>
2024-09-30 12:32:20 +0000

    Create files for mongo schema for planets and launches data

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,30 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
-const {loadPlanetsData, planets} = require('./models/planets.model');
+const {loadPlanetsData} = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGODB_URL = "mongodb+srv://nasaUser:M65ik6wKi5qQ5e0X@basiccluster.ftcop.mongodb.net/nasa?retryWrites=true&w=majority&appName=BasicCluster";
+
 const server = http.createServer(app);
 
-async function startServer() {
-    await loadPlanetsData();
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready');
+});
 
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
+async function startServer() {
+    await mongoose.connect(MONGODB_URL);
+
+    await loadPlanetsData();
+    
 server.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}...`);
     
 })
 }
-
 startServer();
