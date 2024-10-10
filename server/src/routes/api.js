f8453d0fdbbd9389b0dedafa61133ab8fe32eb77
ohenekwabena <adjeiasarejesse@gmail.com>
2024-10-10 00:15:07 +0000

    Implement api versioning

---
+++ server/src/routes/api.js
@@ -0,0 +1,12 @@
+// versioning api endpoint 
+const express = require('express');
+
+const planetsRouter = require('./planets/planets.router');
+const launchesRouter = require('./launches/launches.router');
+
+const api = express.Router();
+
+api.use('/planets', planetsRouter); 
+api.use('/launches', launchesRouter);
+
+module.exports = api;
